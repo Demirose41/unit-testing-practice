2160cba839a45c3e624d9cbad5b7f47c62f0d4e4
demirose41 <demirose41@gmail.com>
2024-10-04 22:58:53 -0700

    1: completed practice  exxersize

--- funcs.js
+++ funcs.js
@@ -1,13 +1,20 @@
 function isFive(num) {
-  // Your code here
+  return num === 5 ? true : false;
 }
 
 function isOdd(number) {
-  // Your code here
+  if(typeof number !== "number") {
+    Error.throw("FUCKER WE ONLY TAKE IN NUMBERS!!");
+  }
+  return number % 2 === 0 ? false : true;
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let range = [];
+  for(let i = min; i <= max; i += step){
+    range.push(i);
+  }
+  return range;
 }
 
 
